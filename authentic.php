2fa98a20bfa8e4b6f010d94c3eae53e4d31573d0
Soumya Tarafder <63846042+Soumya624@users.noreply.github.com>
2020-12-30 03:48:46 +0530

    Update authentic.php

--- authentic.php
+++ authentic.php
@@ -5,14 +5,15 @@
     $password = $_POST['password']; 
      
         //to prevent from mysqli injection 
+/*
         $username = stripcslashes($username); 
         $password = stripcslashes($password); 
         $username = mysqli_real_escape_string($conn, $username); 
         $password = mysqli_real_escape_string($conn, $password); 
-     
+*/     
         $sql = "select *from users where Username = '$username' and Password = '$password'"; 
         $result = mysqli_query($conn, $sql); 
-        $row = mysqli_fetch_array($result, MYSQLI_ASSOC); 
+        // $row = mysqli_fetch_array($result, MYSQLI_ASSOC); 
         $count = mysqli_num_rows($result); 
          echo("<br>");
         if($count == 1){ 
